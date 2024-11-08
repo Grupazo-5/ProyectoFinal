c9a8fc2db97925db125ce1ba589032a3d8e3611f
SrLachy <ignacioareosa07@gmail.com>
2024-11-07 21:56:25 -0300

    cambio de base de datos

--- Programacion/conectar_db/conectar_db.py
+++ Programacion/conectar_db/conectar_db.py
@@ -7,7 +7,7 @@ def conectar_db():
             host="localhost",
             user="root",
             password="123456",
-            database="broker_ispc4"
+            database="broker_ispc"
         )
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
